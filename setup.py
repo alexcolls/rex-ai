a7008b7f9c7d92c2910621d6ec5bf8bba11e2d57
Paul Bogumilo <paul@bogumilo.co>
2022-08-29 15:19:48 +0200

    feat: pauls notebook

---
+++ setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup
+from setuptools import find_packages
+
+# list dependencies from file
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(
+    #   name='model',
+    #   description="Model with code",
+      packages=find_packages(),
+      install_requires=requirements
+    #   packages=["model"])
+    )
