6041e94bb5426eaeda9d120a660d3493033fdd69
quantium-rock <alexcollsoutumuro@gmail.com>
2022-08-30 12:42:45 +0200

    init

--- db/bin/update_db.py
+++ db/bin/update_db.py
@@ -18,17 +18,15 @@ def updateDB():
 
     if primaryData.missing_years:
         # user confirmation
-        input('\nUpdate primary database?\n> Press Enter to proceed\n\n>>> ')
+        input('\nUpdate database?\n> Press Enter to proceed\n\n>>> ')
         primaryData.updateDB()
     
     print('\n### SECONDARY DB ###')
     secondaryData.checkDB()
-    print('\n> Updating secondary data...')
     secondaryData.updateDB()
 
     print('\n### TERTIARY DB ###')
     tertiaryData.checkDB()
-    print('\n> Updating tertiary data...')
     tertiaryData.updateDB()
 
 
