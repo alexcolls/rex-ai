45c7d4720254d21bdcd826273fa2df75c970e7fd
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-05 22:52:21 +0200

    finished first model

--- notebooks/alex/models/one.py
+++ notebooks/alex/models/one.py
@@ -11,14 +11,18 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import OneHotEncoder
 from keras.callbacks import EarlyStopping
 
-import plotly.express as px
-
-
+EPOCHS = 10
+NEURONS = 100
+TRAIN_YEAR = 2010
+VALID_YEAR = 2015
+TEST_YEAR = 2016
+FINAL_YEAR = 2020
+THRESHOLD = 0.05
 
 def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=0.05 ):
 
     # load target
-    y = pd.read_csv(f'../../db/data/merge/secondary/logs_.csv', index_col=0)
+    y = pd.read_csv(f'../../../db/data/merge/secondary/logs_.csv', index_col=0)
     y.index = pd.to_datetime(y.index)
     y = y.replace([np.inf, -np.inf, np.nan], 0)
     y = y[symbol].loc[str(start_year)+'-01-01':str(final_year)+'-12-31']
@@ -32,13 +36,13 @@ def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=0.0
         else:
             return 0
     
-    y = y.map(lambda x: condition(x, threshold)).to_numpy()
+    y = y.map(lambda x: condition(x, THRESHOLD)).to_numpy()
 
     encoder = OneHotEncoder(sparse = False)
     y = encoder.fit_transform(y.reshape(-1,1))
 
     # load features
-    X = pd.read_csv(f'../../db/data/merge/tendency/tendency.csv', index_col=0)
+    X = pd.read_csv(f'../../../db/data/merge/tendency/tendency.csv', index_col=0)
     X.index = pd.to_datetime(X.index)
     X = X.loc[str(start_year)+'-01-01':str(final_year)+'-12-31']
     X = X.filter(regex=f'{symbol[:3]}|{symbol[4:]}|sin|cos')
@@ -66,6 +70,8 @@ def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=0.0
     for col in X.columns:
         X[col], _ = scaleData(X[col])
 
+    # make windows TODO
+
     X = X.to_numpy()
 
     # shift 1 X and y
@@ -74,12 +80,11 @@ def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=0.0
 
     return y, X
 
-y_train, X_train = prepData('EUR_USD', 2010, 2015, 0.05)
-y_val, X_val = prepData('EUR_USD', 2015, 2015, 0.05)
-x_test, y_test = prepData('EUR_USD', 2016, 2020, 0.05)
+y_train, X_train = prepData('EUR_USD', TRAIN_YEAR, VALID_YEAR, THRESHOLD)
+y_val, X_val = prepData('EUR_USD', VALID_YEAR, TEST_YEAR-1, THRESHOLD)
+y_test, X_test = prepData('EUR_USD', TEST_YEAR, FINAL_YEAR, THRESHOLD)
 
-
-def prepModel(X , y, neurons=100):
+def prepModel(X , y, neurons=NEURONS):
 
     model = Sequential()
     model.add(LSTM(neurons, activation='relu', input_shape=(X.shape[1], 1)))
@@ -87,18 +92,24 @@ def prepModel(X , y, neurons=100):
     model.add(layers.Dense(neurons , activation='relu'))
     model.add(Dense(y.shape[-1], activation='softmax',))
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
 
     return model
 
-
 model = prepModel(X_train , y_train)
 
-model.summary()
+def trainModel( X , y, epochs=EPOCHS):
 
-EPOCHS = 100
+    early_stopping = EarlyStopping(monitor='accuracy', patience=24, mode='min')
 
-early_stopping = EarlyStopping(monitor='accuracy', patience=24, mode='min')
+    history = model.fit(X , y, epochs=epochs, callbacks=[early_stopping])
 
-model.fit(X_train , np.array(y_train), epochs=EPOCHS, callbacks=[early_stopping])
+    model.save('one.h5')
 
-model.save('lstm_model.h5')
+    return history 
+
+history = trainModel(X_train, y_train)
+
+results = model.evaluate(X_test, y_test, batch_size=128)
+
+print("test loss, test acc:", results)
