45cca0d5bca2faaefab276f010e482bed155c9ff
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-06 20:10:23 +0200

    updated model2 m2.py

--- notebooks/alex/models/m2/m2.py
+++ notebooks/alex/models/m2/m2.py
@@ -7,27 +7,30 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras import layers
 from pandas import Series
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from keras.callbacks import EarlyStopping
 from keras import models
 import matplotlib.pyplot as plt
 
-
+# LSTM model parameters
+LAYERS = 3
+NEURONS = 144
 EPOCHS = 100
-NEURONS = 100
 THRESHOLD = 0.05
 TRAIN_YEAR = 2018
 VALID_YEAR = 2021
-TEST_YEAR = 2020
-FINAL_YEAR = 2020
+TEST_YEAR = 2021
+FINAL_YEAR = 2022
 DB_PATH = '../../../../db/data/'
 SYMBOLS = []
 
 
 def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=THRESHOLD, lookback=120, load_SYMBOLS=False ):
 
-    # load target
+    ### TARGETS ###
+
+    # load history log returns
     y = pd.read_csv(DB_PATH+'merge/secondary/logs_.csv', index_col=0)
 
     if load_SYMBOLS:
@@ -53,64 +56,95 @@ def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=THR
     encoder = OneHotEncoder(sparse = False)
     y = encoder.fit_transform(y.reshape(-1,1))
 
+    ### FEATURES ###
+
     # load features
     X = pd.read_csv(DB_PATH+'merge/tendency/tendency.csv', index_col=0)
     X.index = pd.to_datetime(X.index)
     X = X.loc[str(start_year)+'-01-01':str(final_year)+'-12-31']
-    X = X.filter(regex=f'{symbol[:3]}|{symbol[4:]}|sin|cos')
-    X = X.replace([np.inf, -np.inf, np.nan], 0)
+    
+    # substract infinites and fill nans
+    X.replace([np.inf, -np.inf], np.nan, inplace=True)
+    X.fillna(method='bfill', inplace=True)
+    X.fillna(method='ffill', inplace=True)
+
+    # update model's neurons by number of features on the dataset
+    global NEURONS 
+    NEURONS = len(X.columns)
 
     # scaling features
-    def scaleData ( data ):
-        series = Series(data)
-        # prepare data for normalization
-        values = series.values
-        values = values.reshape((len(values), 1))
-        # train the normalization
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        scaler = scaler.fit(values)
-        #print('Min: %f, Max: %f' % (scaler.data_min_, scaler.data_max_))
-        # normalize the dataset and print
-        normalized = scaler.transform(values)
-        #print(normalized)
-        # inverse transform and print
-        inversed = scaler.inverse_transform(normalized)
-        #print(inversed)
+    def scaleData ( x ):
 
+        x_ = pd.Series(x.copy())
+        x_ = x_.values
+        x_ = x_.reshape((len(x_), 1))
+        # standard scaler
+        scaler = StandardScaler()
+        scaler = scaler.fit(x_)
+        #print('Min: %f, Max: %f' % (scaler.data_min_, scaler.data_max_))
+        normalized = scaler.transform(x_)
+        #print(normalized)
+        inversed = scaler.inverse_transform(x_)
+        #print(inversed)
         return normalized, inversed
 
+    # scale all columns
     for col in X.columns:
         X[col], _ = scaleData(X[col])
 
-    # make windows
-    def makeWindows ( x, periods=lookback ):
-        df = x.copy()
-        for col in df.columns:
-            for i, row in enumerate(df.index):
-                try:
-                    df[col][row] = list(df.iloc[i-periods:i])
-                except:
-                    df[col][row] = 0
-        return df
+    # make sequences
+    def makeSequences( x, lookback=lookback ):
+
+        out_ = []
+        for i in range(len(x)):
+            seq = 0
+            try:
+                seq = np.array(x.iloc[ i : i-lookback ])
+            except: 
+                pass
+            out_.append(seq)
+
+        out_ = np.array(out_, dtype="object")
         
-    X = makeWindows(X)
+        return out_
+
+    for col in X.columns:
+        X[col] = makeSequences(X[col])
+
     X = X.to_numpy()
 
-    # shift 1 X and y
+    # cut first loockback periods
+    X = X[lookback:]
+    y = y[lookback:]
+
+    # shift 1 X and y (current data to predict next period)
     y = y[:-1]
     X = X[1:]
 
     return y, X
 
 
-def prepModel(X , y, neurons=NEURONS):
+def loadModel ( X , y, layers=LAYERS, neurons=NEURONS, dropout=0.2 ):
+
+    neurons = int( neurons * ( 1 + dropout ) + 1 )
+
+    print('\n', neurons, 'neurons...')
 
     model = Sequential()
-    model.add(LSTM(neurons, activation='relu', input_shape=(X.shape[1], 1)))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(neurons , activation='relu'))
+
+    model.add(LSTM(neurons, activation='relu', return_sequences=False, input_shape=(X.shape[1], 1)))
+    model.add(layers.Dropout(dropout))
+
+    print('\n', layers, 'layers...')
+
+    for i in range(layers):
+        model.add(layers.Dense(neurons , activation='relu'))
+        model.add(layers.Dropout(dropout))
+
     model.add(Dense(y.shape[-1], activation='softmax',))
+
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
     model.summary()
 
     return model
@@ -165,12 +199,12 @@ if __name__ == "__main__":
         model = None
         if not params:
 
-            print('\n> Loading and preprocessing data...')
+            print('\n> Loading and preprocessing data... be patient..')
             # loading and preparing data
             y_train, X_train = prepData(sym, TRAIN_YEAR, VALID_YEAR)
             y_test, X_test = prepData(sym, VALID_YEAR, TEST_YEAR)
 
-            model = prepModel(X_train , y_train)
+            model = loadModel(X_train , y_train)
 
             # fit model
             history = trainModel(X_train, y_train, sym)
