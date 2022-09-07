356f97ee725cd185661695ec6b44a2d997ae621d
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-07 14:21:19 +0200

    oanda_api.py v.1.5

--- db/bin/apis/oanda_api.py
+++ db/bin/apis/oanda_api.py
@@ -55,7 +55,6 @@ class OandaApi:
 
         self.accounts = self.getAccounts()
 
-
     
     # return a list of all the api accounts ids
     def getAccounts( self ):
@@ -76,29 +75,105 @@ class OandaApi:
         
         return accs
 
-
      
-    # return a list of all the api accounts ids
-    def getAccounts( self ):
+    # return the account state (NAV, PnL, margin, accCurrency, etc)
+    def getSummary( self, account_id ):
         
+        if account_id is None:
+            account_id = self.accounts[0]
+
         req = 0
         try:
             req = self.client.get(
-                f"{self.enviroment}/{self.api_version}/accounts"
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/summary"
             )
-            req = json.loads(req.content.decode("utf-8"))['accounts']
+            return json.loads(req.content.decode("utf-8"))['accounts']
         except:
             print("OANDA API ERROR", Exception)
-            return
 
-        accs = []
-        for x in req:
-            accs.append(x['id'])
+    
+    # return a json with all tradeable instruments for a given accoount
+    def getInstruments( self, account_id ):
         
-        return accs
+        if account_id is None:
+            account_id = self.accounts[0]
+
+        req = 0
+        try:
+            req = self.client.get(
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/instruments"
+            )
+            return json.loads(req.content.decode("utf-8"))['instruments']
+        except:
+            print("OANDA API ERROR", Exception)
+
+    
+    # return a list of the current trading positions for a given account
+    def getOpenPositions( self, account_id ):
+        
+        if account_id is None:
+            account_id = self.accounts[0]
+
+        req = 0
+        try:
+            req = self.client.get(
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/openPositions"
+            )
+            return json.loads(req.content.decode("utf-8"))['positions']
+        except:
+            print("OANDA API ERROR", Exception)
 
 
-    # return json with history candles between 2 dates
+    
+    # return a list with the PnLs of each instrument for a given account
+    def getAllPositions( self, account_id ):
+        
+        if account_id is None:
+            account_id = self.accounts[0]
+
+        req = 0
+        try:
+            req = self.client.get(
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/positions"
+            )
+            return json.loads(req.content.decode("utf-8"))['positions']
+        except:
+            print("OANDA API ERROR", Exception)
+
+
+    # return a list of the current open trades for a given account
+    def getOpenTrades( self, account_id ):
+        
+        if account_id is None:
+            account_id = self.accounts[0]
+
+        req = 0
+        try:
+            req = self.client.get(
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/openTrades"
+            )
+            return json.loads(req.content.decode("utf-8"))['trades']
+        except:
+            print("OANDA API ERROR", Exception)
+
+
+    # return a list of all historical trades of the account
+    def getAllTrades( self, account_id ):
+        
+        if account_id is None:
+            account_id = self.accounts[0]
+
+        req = 0
+        try:
+            req = self.client.get(
+                f"{self.enviroment}/{self.api_version}/accounts/{account_id}/openTrades"
+            )
+            return json.loads(req.content.decode("utf-8"))['trades']
+        except:
+            print("OANDA API ERROR", Exception)
+
+
+    # return json with history candles between 2 dates (max periods=5000)
     def getCandles( self, symbol, timeframe, start_date, count=5000, include_frist=False, mids=True ):
 
         prices = "M" if mids else "BA"  # Mids or BidAsks
@@ -113,13 +188,10 @@ class OandaApi:
             print("OANDA API ERROR", Exception)
     
 
-
-
-    # return 
-
     # ORDER EXECUTOR
 
-    # TODO
+    def postOrder ( self ):
+        return
 
 
 # END
