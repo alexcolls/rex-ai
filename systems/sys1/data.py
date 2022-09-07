98ced8dff5f19179421f2ee4e39db783d85617eb
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-08 00:52:33 +0200

    updated sys1

--- systems/sys1/data.py
+++ systems/sys1/data.py
@@ -2,8 +2,10 @@
 # license: MIT
 
 import os
+import math
 import numpy as np
 import pandas as pd
+import scipy.signal as sig
 from pathlib import Path
 from apis.oanda_api import OandaApi
 from config import SYMBOLS, TIMEFRAME, LOOKBACK
@@ -184,8 +186,173 @@ class DataSet:
         return ccys
 
 
+    ### INDICATORS ###
 
-# main for function call
+    def time_standard( self, df ):
+        # Spliting Datetime
+        data = pd.DataFrame([])
+        data.index = pd.to_datetime(df.index) # type: ignore
+
+        data["month_sin"] = np.sin((np.array(data.index.month)*math.pi*2)/12)
+        data["month_cos"]=np.cos((np.array(data.index.month)*math.pi*2)/12)
+        data["day_sin"] = np.sin((np.array(data.index.day_of_year)*math.pi*2)/360)
+        data["day_cos"]=np.cos((np.array(data.index.day_of_year)*math.pi*2)/360)
+        data["weekday_sin"] = np.sin((np.array(data.index.weekday)*math.pi*2)/7)
+        data["weekday_cos"]=np.cos((np.array(data.index.weekday)*math.pi*2)/7)
+        data["hour_sin"] = np.sin((np.array(data.index.hour)*math.pi*2)/24)
+        data["hour_cos"]=np.cos((np.array(data.index.hour)*math.pi*2)/24)
+        data.index = df.index
+
+        return data
+
+    def correlations(df, rate=120):
+        data = pd.DataFrame([])
+        for sym in SYMBOLS:
+            data[f'{sym[:3]}_{sym[4:]}_corr'] = df[f'{sym[:3]}'].rolling(rate).corr(df[f'{sym[4:]}'])
+        data.index = df.index
+        return data
+
+    def get_bollinger_bands( self, df, rate=5, sigma_start=2, sigma_stop=2):
+        """regular bollinger bands, do not apply to a RNN, use bollinger_small"""
+        sma = df.rolling(rate).mean()
+        bollinger_bands = {"sma":sma}
+        std = df.rolling(rate).std()
+        for sigma in range(sigma_start, sigma_stop+1):
+            up = sma + std * sigma
+            down = sma - std * sigma
+            bollinger_bands[f'up{sigma}'] = up
+            bollinger_bands[f'down{sigma}'] = down
+        bol = pd.DataFrame.from_dict(bollinger_bands)
+        bol.set_index(df.index, inplace=True)
+        return bol
+
+    def bollinger_small( self, df, rate=24, sigma_start=2, sigma_stop=2):
+
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            data[f'{currency}_sma'] = df[currency].rolling(rate).mean()
+            data[f'{currency}_std']= df[currency].rolling(rate).std()
+        data.index = df.index
+
+        return data
+
+    def volatility( self, df, rate=240, window=506):
+
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            data[f"{currency}_vol1"] = df[currency].rolling(rate).std()/(window**0.5)
+            data[f"{currency}_vol2"] = df[currency].rolling(rate).std()*(window**0.5)
+        data.index = df.index
+
+        return data
+
+    def sharpe_ratio(self, df, window=24):
+
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            data[f'{currency}_sharpe'] = df[currency].rolling(window).mean()/df[currency].rolling(window).std()
+        data.index = df.index
+
+        return data
+
+    def atr( self, idxs: pd.DataFrame, low: pd.DataFrame, high: pd.DataFrame, window:int=14):
+
+        data = pd.DataFrame([])
+
+        for currency in idxs.columns:
+            data[f'idxs_{currency}'] = idxs[currency]
+            data[f'high_{currency}'] = high[currency]
+            data[f'low_{currency}'] = low[currency]
+            data[f'tr0_{currency}'] = data[f'high_{currency}'] - data[f'low_{currency}']
+            data[f'tr1_{currency}'] = np.abs(data[f'high_{currency}'] - data[f'idxs_{currency}'].shift())
+            data[f'tr2_{currency}'] = np.abs(data[f'low_{currency}'] - data[f'idxs_{currency}'].shift())
+            data[f'tr_{currency}'] = data[[f'tr0_{currency}', f'tr1_{currency}', f'tr2_{currency}']].max(axis=1)
+            data[f'atr_{currency}'] = data[f'tr_{currency}'].ewm(alpha=1/window, adjust=False).mean()
+            data.drop(columns=[f'idxs_{currency}',f'tr0_{currency}',f'tr1_{currency}',f'tr2_{currency}',f'tr_{currency}'], inplace=True)
+        
+        return data
+
+    def ema( self, df, window=48):
+        """
+        Exponential movint average
+        """
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            data[f'{currency}_ema{window}'] = df[currency].ewm(alpha=1/window, adjust=False).mean()
+        data.index = df.index
+
+        return data
+
+
+    def highpass_filter( self, df, order=5, cutoff=0.2):
+
+        data = pd.DataFrame([])
+        b, a = sig.butter(N=order, Wn=cutoff, btype='highpass', analog=False)
+        for currency in df.columns:
+            data[f'{currency}_highpass'] = sig.lfilter(b, a, df[currency])
+        data.index = df.index
+
+        return data
+
+    def lowpass_filter( self, df, order=8, cutoff=0.2 ):
+        
+        data = pd.DataFrame([])
+        b, a = sig.butter(N=order, Wn=cutoff, btype='lowpass', analog=False)
+        for currency in df.columns:
+            data[f'{currency}_lowpass'] = sig.lfilter(b, a, df[currency])
+        data.index = df.index
+
+        return data
+
+    def rsi( self, df, periods = 240 ):
+        
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            delta = df[currency].diff()
+            up = delta.clip(lower=0)
+            down = -1*delta.clip(upper=0)
+            ema_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+            ema_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+            rs = ema_up / ema_down
+            rsi = 100 - (100/(1 + rs))
+            data[f'{currency}_rsi{periods}'] = rsi
+
+        return data
+
+    def lowpass_momentum( self, df ):
+
+        data = pd.DataFrame([])
+        low = self.lowpass_filter(df)
+        low2 = low.shift(1)
+        for currency in low.columns:
+            data[f'{currency}_hp_momentum'] = low2[currency] - low[currency]
+        data.index = df.index
+        data.fillna(0)
+        return data
+
+    def highpass_momentum( self, df ):
+
+        data = pd.DataFrame([])
+        high = self.highpass_filter(df)
+        high2 = high.shift(1)
+        for currency in high.columns:
+            data[f'{currency}_hp_momentum'] = high2[currency] - high[currency]
+        data.index = df.index
+        data.fillna(0, inplace=True)
+        return data
+
+    def ema_diff( df, window=8 ):
+
+        data = pd.DataFrame([])
+        for currency in df.columns:
+            data[f'{currency}_ema{window}_diff'] = df[currency].ewm(alpha=1/window, adjust=False).mean() - df[currency]
+        data.index = df.index
+        data.fillna(0, inplace=True)
+
+        return data
+
+
+
 if __name__ == "__main__":
 
     data = DataSet()
@@ -193,7 +360,5 @@ if __name__ == "__main__":
     op, hi, lo, cl, vo = data.getCandles()
     logs, rets, vols, higs, lows = data.normalizeData(op, hi, lo, cl, vo)
     logs_, rets_, vols_, higs_, lows_, idxs_ = data.reduceDimension(logs, rets, vols, higs, lows)
-
-    
 
 
