ffc7465ca3133a93d90411f757a992cfcf9735c3
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-07 12:07:26 +0200

    finished m2.py

--- notebooks/alex/models/m2/m2.py
+++ notebooks/alex/models/m2/m2.py
@@ -1,4 +1,5 @@
 
+
 import os.path
 import numpy as np
 import pandas as pd
@@ -11,6 +12,9 @@ from keras.models import load_model
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 import matplotlib.pyplot as plt
+import pickle
+
+
 
 # LSTM model parameters
 LAYERS = 3
@@ -18,30 +22,24 @@ NEURONS = 144
 LOOKBACK = 120
 EPOCHS = 100
 THRESHOLD = 0.05
-TRAIN_YEAR = 2022
-VALID_YEAR = 2022
-TEST_YEAR = 2002
+TRAIN_YEAR = 2018
+VALID_YEAR = 2021
+TEST_YEAR = 2022
 FINAL_YEAR = 2022
 DB_PATH = '../../../../db/data/'
 SYMBOLS = []
 
-
 def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=THRESHOLD, lookback=LOOKBACK, load_SYMBOLS=False ):
-
     ### TARGETS ###
-
     # load history log returns
     y = pd.read_csv(DB_PATH+'merge/secondary/logs_.csv', index_col=0)
-
     if load_SYMBOLS:
         global SYMBOLS
         SYMBOLS = y.columns
         return 0
-
     y.index = pd.to_datetime(y.index)
     y = y.replace([np.inf, -np.inf, np.nan], 0)
     y = y[symbol].loc[str(start_year)+'-01-01':str(final_year)+'-12-31']
-
     # transform target to a classification (1, 0, -1)
     def condition(x, threshold):
         if x > threshold:
@@ -50,77 +48,47 @@ def prepData ( symbol='EUR_USD', start_year=2010, final_year=2015, threshold=THR
             return -1
         else:
             return 0
-    
     y = y.map(lambda x: condition(x, threshold)).to_numpy()
-
     encoder = OneHotEncoder(sparse = False)
     y = encoder.fit_transform(y.reshape(-1,1))
-
     ### FEATURES ###
-
     # load features
     X = pd.read_csv(DB_PATH+'merge/tendency/tendency.csv', index_col=0)
     X.index = pd.to_datetime(X.index)
     X = X.loc[str(start_year)+'-01-01':str(final_year)+'-12-31']
-    
     # substract infinites and fill nans
     X.replace([np.inf, -np.inf], np.nan, inplace=True)
     X.fillna(method='bfill', inplace=True)
     X.fillna(method='ffill', inplace=True)
-
     # update model's neurons by number of features on the dataset
-    global NEURONS 
+    global NEURONS
     NEURONS = len(X.columns)
-
     # scaling features
-    def scaleData ( x ):
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
 
-        x_ = pd.Series(x.copy())
-        x_ = x_.values
-        x_ = x_.reshape((len(x_), 1))
-        # standard scaler
-        scaler = StandardScaler()
-        scaler = scaler.fit(x_)
-        #print('Min: %f, Max: %f' % (scaler.data_min_, scaler.data_max_))
-        normalized = scaler.transform(x_)
-        #print(normalized)
-        inversed = scaler.inverse_transform(x_)
-        #print(inversed)
-        return normalized, inversed
-
-    # scale all columns
-    for col in X.columns:
-        X[col], _ = scaleData(X[col])
+    # Export Pipeline as pickle file
+    with open("scaler.pkl", "wb") as file:
+        pickle.dump(scaler, file)
 
     # make sequences
-    def makeSequences( x, lookback=lookback ):
-
-        x_ = x.copy().to_numpy()
-        out_ = []
-        for i in range(len(x_)):
-            seq = 0
+    def makeSequences( X, y, lookback=lookback ):
+        X = pd.DataFrame(X)
+        y = pd.DataFrame(y)
+        n_days = lookback
+        X_tensor = []
+        y_tensor = []
+        for index in range(n_days, X.shape[0]):
             try:
-                seq = x_[ i-lookback : i ]
-            except: 
-                pass
-            out_.append(seq)
-        
-        return out_
+                X_tensor.append(X.iloc[index - n_days:index])
+                y_tensor.append(y.iloc[index+1])
+            except:
+                break
+        X_tensor = np.array(X_tensor[:-1])
+        y_tensor = np.array(y_tensor)
+        return X_tensor, y_tensor
 
-    for col in X.columns:
-        X[col] = makeSequences(X[col])
-
-    X = X.to_numpy()
-
-    # cut first loockback periods
-    X = X[lookback+1:]
-    y = y[lookback+1:]
-
-    # shift 1 X and y (current data to predict next period)
-    y = y[:-1]
-    X = X[1:]
-
-    return y, X
+    return makeSequences( X, y, lookback=lookback )
 
 
 def buildModel ( X , y, layers=LAYERS, neurons=NEURONS, dropout=0.2 ):
@@ -135,7 +103,7 @@ def buildModel ( X , y, layers=LAYERS, neurons=NEURONS, dropout=0.2 ):
 
     model = Sequential()
 
-    model.add( LSTM(neurons, activation='relu', return_sequences=False, input_shape=(X.shape[1], 1)) )
+    model.add( LSTM(neurons, activation='tanh', return_sequences=False, input_shape=(X.shape[1], X.shape[2])) )
 
     model.add( Dropout(dropout) )
 
@@ -157,11 +125,11 @@ def buildModel ( X , y, layers=LAYERS, neurons=NEURONS, dropout=0.2 ):
     return model
 
 
-def trainModel ( model, X, y, symbol, epochs=EPOCHS, plot=False):
+def trainModel ( model, X, y, X_val, y_val, symbol, epochs=EPOCHS, plot=False):
 
-    early_stopping = EarlyStopping(monitor='accuracy', patience=2, mode='min')
+    early_stopping = EarlyStopping(monitor='accuracy', patience=24, mode='min', restore_best_weights=True)
 
-    history = model.fit(np.asarray(X , dtype = np.float32) , y, epochs=epochs, batch_size=LOOKBACK, callbacks=[early_stopping], verbose=2)
+    history = model.fit(X , y, epochs=epochs, batch_size=LOOKBACK, verbose=1, callbacks=[early_stopping], validation_data=(X_val, y_val))
 
     model.save(__file__[:-3]+'_'+symbol+'.h5')
 
@@ -209,19 +177,19 @@ if __name__ == "__main__":
 
             print('\n> Loading and preprocessing data...\n')
             # loading and preparing data
-            y_train, X_train = prepData(sym, TRAIN_YEAR, VALID_YEAR-1)
-            y_valid, X_valid = prepData(sym, VALID_YEAR, VALID_YEAR)
+            X_train, y_train = prepData(sym, TRAIN_YEAR, VALID_YEAR-1)
+            X_valid, y_valid = prepData(sym, VALID_YEAR, VALID_YEAR)
 
             print(y_train.shape, X_train.shape)
 
             model = buildModel(X_train , y_train)
 
             # fit model
-            history = trainModel(model, X_train, y_train, sym)
+            history = trainModel(model, X_train, y_train, X_valid, y_valid, sym)
 
             print('\n')
             # test model
-            results = model.evaluate(X_valid, y_valid, batch_size=LOOKBACK)
+            results = model.evaluate(X_valid, y_valid)
 
             print('\n')
             print('test loss:', round(results[0],2), 'test accuracy:', round(results[1],2))
@@ -229,8 +197,8 @@ if __name__ == "__main__":
 
         else:
 
-            y_val, X_val = prepData(sym, VALID_YEAR, TEST_YEAR-1)
-            y_test, X_test = prepData(sym, TEST_YEAR, FINAL_YEAR)
+            X_val, y_val = prepData(sym, VALID_YEAR, TEST_YEAR-1)
+            X_test, y_test = prepData(sym, TEST_YEAR, FINAL_YEAR)
 
             model = load_model(__file__[:-3]+'_'+sym+'.h5')
 
