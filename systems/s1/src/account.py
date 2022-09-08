ddb1b98bc39c32674753d2196d9c57cc3efb8ba1
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-08 12:44:58 +0200

    systems/s1/accounts.py

--- systems/s1/src/account.py
+++ systems/s1/src/account.py
@@ -2,19 +2,68 @@
 from config import ACCOUNT
 from apis.oanda_api import OandaApi
 
-oa = OandaApi()
 
-summary = oa.getSummary(ACCOUNT)
+def getAccount():
 
-account = {}
-account['dt'] = summary['createdTime']
-account['id'] = summary['id']
-account['ccy'] = summary['currency']
-account['balance'] = round(float(summary['balance']),2)
-account['close_pnl'] = round(float(summary['resettablePL']),2)
-account['NAV'] = round(float(summary['NAV']),2)
-account['open_pnl'] = round(float(summary['unrealizedPL']),2)
-account['commissions'] = round(float(summary['financing']),2)
-account['margin_used'] = round(float(summary['marginUsed']),2)
-account['open_position'] = round(float(summary['positionValue']),2)
-account['leverage'] = round(account['open_position'] / account['NAV'],2)
+    oa = OandaApi()
+
+    summary = oa.getSummary(ACCOUNT)
+
+    account = {}
+    account['dt'] = summary['createdTime']
+    account['id'] = summary['id']
+    account['ccy'] = summary['currency']
+    account['balance'] = round(float(summary['balance']),2)
+    account['close_pnl'] = round(float(summary['resettablePL']),2)
+    account['NAV'] = round(float(summary['NAV']),2)
+    account['open_pnl'] = round(float(summary['unrealizedPL']),2)
+    account['commissions'] = round(float(summary['financing']),2)
+    account['margin_used'] = round(float(summary['marginUsed']),2)
+    account['open_position'] = round(float(summary['positionValue']),2)
+    account['leverage'] = round(account['open_position'] / account['NAV'],2)
+
+    return account
+
+
+def getPositions():
+
+    oa = OandaApi()
+
+    summary = oa.getSummary(ACCOUNT)
+
+    account = {}
+    account['dt'] = summary['createdTime']
+    account['id'] = summary['id']
+    account['ccy'] = summary['currency']
+    account['balance'] = round(float(summary['balance']),2)
+    account['close_pnl'] = round(float(summary['resettablePL']),2)
+    account['NAV'] = round(float(summary['NAV']),2)
+    account['open_pnl'] = round(float(summary['unrealizedPL']),2)
+    account['commissions'] = round(float(summary['financing']),2)
+    account['margin_used'] = round(float(summary['marginUsed']),2)
+    account['open_position'] = round(float(summary['positionValue']),2)
+    account['leverage'] = round(account['open_position'] / account['NAV'],2)
+
+    return account
+
+
+def getExposure():
+
+    oa = OandaApi()
+
+    summary = oa.getSummary(ACCOUNT)
+
+    account = {}
+    account['dt'] = summary['createdTime']
+    account['id'] = summary['id']
+    account['ccy'] = summary['currency']
+    account['balance'] = round(float(summary['balance']),2)
+    account['close_pnl'] = round(float(summary['resettablePL']),2)
+    account['NAV'] = round(float(summary['NAV']),2)
+    account['open_pnl'] = round(float(summary['unrealizedPL']),2)
+    account['commissions'] = round(float(summary['financing']),2)
+    account['margin_used'] = round(float(summary['marginUsed']),2)
+    account['open_position'] = round(float(summary['positionValue']),2)
+    account['leverage'] = round(account['open_position'] / account['NAV'],2)
+
+    return account
