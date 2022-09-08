fe451f820a42eef2483aa41e5c6747ea273c774d
quantium-rock <alexcollsoutumuro@gmail.com>
2022-09-08 17:50:07 +0200

    systems/s1 v0.88

--- systems/s1/src/risk_manager.py
+++ systems/s1/src/risk_manager.py
@@ -10,30 +10,31 @@ from account import Account
 with open('config.json') as json_file:
     config = json.load(json_file)
 
-RISK = config['RISK']
 BALANCE = config['BALANCE']
-LEVERAGE = config['LEVERAGE']
+RISK = config['RISK_ALLOWANCE']
+LEVERAGE = config['MAX_LEVERAGE']
 
 
 class RiskManager( Account, Predictions ):
 
     def __init__( self, risk=RISK, balance=BALANCE, leverage=LEVERAGE ):
 
-        super().__init__()
+        super(Account, self).__init__()
+        super(Predictions, self).__init__()
+
         self.risk = risk
         self.balance = balance
         self.leverage = leverage
-        self.volatilities, _ = self.mean_volatility_prediction()
+        self.volatilities, _ = self.mean_volatility_prediction(self.logs)
         self.correlations = self.correlation_pairs(self.logs)
         self.variance, self.exp_volatilies = self.expected_volatility(self.volatilities, self.correlations)
         self.wei_volatilities = self.weighted_volatility(self.exp_volatilies)
-        self.fx_rates = self.exchange_rates(self.fx_rates)
 
 
-    def mean_volatility_prediction(self, n_devs=2 ):
+    def mean_volatility_prediction(self, logs, n_devs=2 ):
 
         data = pd.DataFrame([])
-        for ccy in self.logs.columns:
+        for ccy in logs.columns:
             data[ccy] = np.abs(self.logs[ccy]).mean() + self.logs[ccy].std()*n_devs
         data.index = self.logs.index
         data2 = data.iloc[-1:].copy()
@@ -44,6 +45,7 @@ class RiskManager( Account, Predictions ):
     def correlation_pairs(self, logs):
 
         data = pd.DataFrame(index=logs.index)
+
         for pair in logs.columns:
             for second_pair in logs.columns:
                 data[f'{pair}_{second_pair}_corr'] = logs[pair].corr(logs[second_pair])
@@ -90,69 +92,16 @@ class RiskManager( Account, Predictions ):
         return  weight
 
 
-    
-
-    
     def makePositions( self ):
         
-        for x in self.predictions:
-            pass
+        for sym, x in self.predictions.iteritems():
+            print(sym, x)
             
-
-    def exchange_rates(self, df, units):
-
-        rate = df.iloc[-1:]
-
-        data = pd.DataFrame(index=rate.index)
-
-        acc_ccy = self.account['ccy']
-
-        for ccy in self.symbols:
-            if ccy.split("_",2)[0] == acc_ccy:
-                data[(ccy.split("_")[1])] = rate[ccy]
-            elif ccy.split("_",2)[1] == acc_ccy:
-                data[(ccy.split("_")[0])] = 1/rate[ccy]
-
-        return data
-
-
-    def trade_signals(self, pred_vols, logs, classification, exchange_rate ):
-
-        cor = self.correlation_pairs(logs)
-
-        t,v = self.expected_volatility(pred_vols, cor)
-        n = int((abs(classification.T)).sum())
-        risked = self.risk*self.balance*self.leverage/(t*n)
-
-        trade_orders = []
-        trade_dataframe = {"datetime":[],"currency":[], "side":[], "size":[]}
-        for ccy in classification.columns:
-            d = {}
-            if int(classification[ccy]) != 0:
-                d["currency"] = ccy
-                trade_dataframe["currency"].append(ccy)
-                signal = "sell" if int(classification[ccy]) == -1 else "buy"
-                trade_dataframe["side"].append(signal)
-
-                if ccy.split("_")[0] == "USD":
-                    d["size"] = round(risked*int(classification[ccy]))
-                    trade_dataframe["size"].append(round(risked))
-                else:
-                    d["size"] = round(risked*float(exchange_rate[ccy.split("_")[0]])*int(classification[ccy]))
-                    trade_dataframe["size"].append(round(risked*float(exchange_rate[ccy.split("_")[0]])))
-                d["datetime"] = v.index.to_list()[0].strftime("%Y-%m-%dT%H:%M:%S.Z")
-                trade_dataframe["datetime"].append(v.index.to_list()[0])
-
-                trade_orders.append(d)
-
-        trade_dataframe = pd.DataFrame.from_dict(trade_dataframe)
-        trade_dataframe.set_index("datetime",inplace=True)
-        trade_dataframe.index = pd.to_datetime(trade_dataframe.index)
-
-        return trade_orders, trade_dataframe
 
 
 if __name__ == "__main__":
 
     rm = RiskManager()
+
+    rm.makePositions()
 
